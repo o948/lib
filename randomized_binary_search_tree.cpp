2834cf495333fc363fe7915ce2440823b338762e
Mindaugas Kazlauskas <mindaugas@canva.com>
2018-05-25 00:29:10 +1000

    simplify randomized_binary_search_tree.cpp

--- randomized_binary_search_tree.cpp
+++ randomized_binary_search_tree.cpp
@@ -2,146 +2,129 @@
 using namespace std;
 
 
-struct bst {
+struct binary_search_tree {
 
 	struct node {
-		int x;
-		int size;
-		int left;
-		int right;
+		const int x;
 
-		node(int x, int left, int right)
-			: x(x), left(left), right(right) {}
+		int size = 1;
+		node* left = nullptr;
+		node* right = nullptr;
+
+		node(int x) : x(x) {}
 	};
 
-	int root = -1;
-	vector<node> vs;
+	node* root = nullptr;
 
-	int new_node(int x, int left, int right) {
-		int r = vs.size();
-		vs.emplace_back(x, left, right);
-		recalc(r);
-		return r;
-	}
-
-	void recalc(int r) {
-		if (r == -1) return;
-		vs[r].size = 1;
-		if (vs[r].left != -1) {
-			vs[r].size += vs[vs[r].left].size;
+	void recalc(node* r) {
+		r->size = 1;
+		if (r->left != nullptr) {
+			r->size += r->left->size;
 		}
-		if (vs[r].right != -1) {
-			vs[r].size += vs[vs[r].right].size;
+		if (r->right != nullptr) {
+			r->size += r->right->size;
 		}
 	}
 
-	void split(int x, int r, int& a, int& b) {
-		if (r == -1) return;
-		if (x <= vs[r].x) {
-			int mid = -1;
-			split(x, vs[r].left, a, mid);
-			vs[r].left = mid;
-			b = r;
+	void split(node* r, int x, node*& lt, node*& ge) {
+		if (r == nullptr) return;
+		if (x <= r->x) {
+			node* tmp = nullptr;
+			split(r->left, x, lt, tmp);
+			ge = r;
+			ge->left = tmp;
+			recalc(ge);
 		} else {
-			int mid = -1;
-			split(x, vs[r].right, mid, b);
-			vs[r].right = mid;
-			a = r;
+			node* tmp = nullptr;
+			split(r->right, x, tmp, ge);
+			lt = r;
+			lt->right = tmp;
+			recalc(lt);
 		}
-		recalc(a);
-		recalc(b);
 	}
 
-	int join(int a, int b) {
-		if (a == -1) return b;
-		if (b == -1) return a;
-		if (rand() % (vs[a].size + vs[b].size) < vs[a].size) {
-			vs[a].right = join(vs[a].right, b);
-			recalc(a);
-			return a;
+	void split(node* r, int x, node*& lt, node*& eq, node*& gt) {
+		node* tmp = nullptr;
+		split(root, x, lt, tmp);
+		split(tmp, x+1, eq, gt);
+	}
+
+	node* join(node* r1, node* r2) {
+		if (r1 == nullptr) return r2;
+		if (r2 == nullptr) return r1;
+
+		int k = rand() % (r1->size + r2->size);
+		if (k < r1->size) {
+			r1->right = join(r1->right, r2);
+			recalc(r1);
+			return r1;
 		} else {
-			vs[b].left = join(a, vs[b].left);
-			recalc(b);
-			return b;
+			r2->left = join(r1, r2->left);
+			recalc(r2);
+			return r2;
 		}
 	}
 
 	int size() {
-		return root != -1 ? vs[root].size : 0;
+		return root != nullptr ? root->size : 0;
 	}
 
 	void add(int x) {
-		root = add(x, root);
-	}
+		node* lt = nullptr;
+		node* eq = nullptr;
+		node* gt = nullptr;
+		split(root, x, lt, eq, gt);
 
-	int add(int x, int r) {
-		if (r == -1) {
-			return new_node(x, -1, -1);
+		if (eq == nullptr) {
+			eq = new node(x);
 		}
-		if (rand() % (vs[r].size + 1) == 0) {
-			int a = -1;
-			int b = -1;
-			split(x, r, a, b);
-			return new_node(x, a, b);
-		}
-		if (x < vs[r].x) {
-			int rr = add(x, vs[r].left);
-			vs[r].left = rr;
-		} else {
-			int rr = add(x, vs[r].right);
-			vs[r].right = rr;
-		}
-		recalc(r);
-		return r;
+
+		root = join(join(lt, eq), gt);
 	}
 
 	void del(int x) {
-		root = del(x, root);
-	}
+		node* lt = nullptr;
+		node* eq = nullptr;
+		node* gt = nullptr;
+		split(root, x, lt, eq, gt);
 
-	int del(int x, int r) {
-		if (r == -1) return -1;
-		if (x == vs[r].x) {
-			return join(vs[r].left, vs[r].right);
+		if (eq != nullptr) {
+			delete eq;
+			eq = nullptr;
 		}
-		if (x < vs[r].x) {
-			vs[r].left = del(x, vs[r].left);
-		} else {
-			vs[r].right = del(x, vs[r].right);
-		}
-		recalc(r);
-		return r;
+
+		root = join(join(lt, eq), gt);
 	}
 
 	bool has(int x) {
-		return has(x, root);
+		return has(root, x);
 	}
 
-	bool has(int x, int r) {
-		if (r == -1) return false;
-		if (x < vs[r].x) return has(x, vs[r].left);
-		if (x > vs[r].x) return has(x, vs[r].right);
+	bool has(node* r, int x) {
+		if (r == nullptr) return false;
+		if (x < r->x) return has(r->left, x);
+		if (x > r->x) return has(r->right, x);
 		return true;
 	}
 
 	int kth(int k) {
 		assert(1 <= k && k <= size());
-		return kth(k, root);
+		return kth(root, k);
 	}
 
-	int kth(int k, int r) {
-		int lsize = vs[r].left != -1 ? vs[vs[r].left].size : 0;
+	int kth(node* r, int k) {
+		int lsize = r->left != nullptr ? r->left->size : 0;
 		if (k <= lsize) {
-			return kth(k, vs[r].left);
+			return kth(r->left, k);
 		} else {
 			k -= lsize;
 		}
 		if (k == 1) {
-			return vs[r].x;
+			return r->x;
 		} else {
 			k--;
 		}
-		return kth(k, vs[r].right);
+		return kth(r->right, k);
 	}
 };
 
@@ -152,7 +135,7 @@ int main() {
 	int Q = 1e5;
 
 	set<int> xs;
-	bst tree;
+	binary_search_tree tree;
 
 	while (Q--) {
 		switch (rand()%2) {
@@ -162,10 +145,12 @@ int main() {
 					xs.erase(x);
 					assert(tree.has(x));
 					tree.del(x);
+					assert(!tree.has(x));
 				} else {
 					xs.insert(x);
 					assert(!tree.has(x));
 					tree.add(x);
+					assert(tree.has(x));
 				}
 				break;
 			}
